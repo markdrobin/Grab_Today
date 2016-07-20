a83e6a5caa8c66069500daf24fb51ab23889464b
Mark Robin <markrobin815@gmail.com>
2016-07-20 12:08:07 +0800

    added splash image, refined UI for homepage, and fixed bug in notification

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,39 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+$( document ).ready(function() {
+	if($(".splash").is(":visible"))
+	{
+		$(".wrapper").css({"opacity":"0"});
+	}
+	$(".splash-arrow").click(function()
+	{
+		$(".splash").slideUp("800", function() {
+			  $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+		 });
+	});
+});
+
+$(".closebtn").ready(function() {
+	var close = document.getElementsByClassName("closebtn");
+  var i;
+
+  // Loop through all close buttons
+  for (i = 0; i < close.length; i++) {
+      // When someone clicks on a close button
+      close[i].onclick = function(){
+
+          // Get the parent of <span class="closebtn"> (<div class="alert">)
+          var div = this.parentElement;
+
+          // Set the opacity of div to 0 (transparent)
+          div.style.opacity = "0";
+
+          // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
+          setTimeout(function(){ div.style.display = "none"; }, 600);
+      }
+
+      $(".notifiers").alert();
+        window.setTimeout(function() { $(".notifiers").alert('.closebtn'); }, 2000);
+      }
+});

95a1d42c1127e9a31178f06e59099bdd3d2af2df
Erik John T. Alicaya <imerikshawn@gmail.com>
2016-08-01 16:21:59 +0800

    Implement cancancan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     redirect_to '/sign_in' unless current_user
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
 end
